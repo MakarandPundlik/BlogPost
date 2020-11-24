e47d811e313a422403afa263acb1c5d777ab7585
MakarandPundlik <mmp2308@gmail.com>
2020-11-24 10:58:50 +0530

    added reports on homepage

--- Frontend/src/components/HomePage.js
+++ Frontend/src/components/HomePage.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import Button from '@material-ui/core/Button'
+import axios from 'axios';
+const API_URL = "http://localhost:2020/";
+const styles = {
+	button: {
+		margin: 15,
+		color: '#AC3B61',
+		background: '#ffffff',
+		fontWeight: 'bold',
+    },
+	floatingLabelFocusStyle: {
+		color: "#F9A257"
+	},
+};
 
 const HomePage = () =>{
+
+    const handleClick=(e)=>{
+        e.preventDefault();
+        axios.get(`${API_URL}`,{
+            headers:{
+                Accept:"application/json",
+                "Content-Type":"application/json",
+
+            }
+        })
+        .then((res)=>{
+            console.log(res.data.users);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
     return(
         <div>
-        Welcome to the homepage
+        <Button 
+		onClick={handleClick}
+		style={styles.button}>Report</Button>
         </div>
     )
 }
