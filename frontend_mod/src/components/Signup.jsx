a6789cfb67d26b6890309d1a9a092ca1ac695dc8
MakarandPundlik <mmp2308@gmail.com>
2021-02-15 20:01:30 +0530

    added validation

--- frontend_mod/src/components/Signup.jsx
+++ frontend_mod/src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Validator from '../services/Validator';
 function Signup(props) {
-    let [state, setState] = useState({
+    const [state, setState] = useState({
         email: '',
         password: '',
         conpassword: '',
@@ -9,44 +9,59 @@ function Signup(props) {
         lastname: ''
     })
     const handleChange = (e) => {
-        setState({ ...state, [e.target.id]: e.target.value })
+
+        setState({ ...state, [e.target.id]: e.target.value });
     }
-    const handleSubmit = () =>{
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
         let profile = {};
-        profile.email=state.email;
-        profile.password=state.password;
-        profile.conpassword=state.conpassword;
-        profile
+        profile.email = state.email;
+        profile.password = state.password;
+        profile.conpassword = state.conpassword;
+        profile.firstname = state.firstname;
+        profile.lastname = state.lastname;
+
+        const auth = Validator(profile);
+        alert(auth.msg);
+        setState({
+            email: '',
+            password: '',
+            conpassword: '',
+            firstname: '',
+            lastname: ''
+        });
     }
     return (
         <div>
-            <div className="container col-md-3 col-sm-8 my-5" onChange={() => handleChange}>
+            <div className="container col-md-3 col-sm-8 my-5">
+                
                 <h3>Sign Up</h3>
-                <form>
-                    <div class="mb-3">
-                        <label class="form-label">First Name</label>
-                        <input type="text" class="form-control" id="firstname" />
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <div className="mb-3">
+                        <label className="form-label">First Name</label>
+                        <input type="text" className="form-control" id="firstname" value={state.firstname} onChange={(e) => handleChange(e)} />
 
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Last Name</label>
-                        <input type="text" class="form-control" id="lastname" />
+                    <div className="mb-3">
+                        <label className="form-label">Last Name</label>
+                        <input type="text" className="form-control" id="lastname" value={state.lastname} onChange={(e) => handleChange(e)} />
 
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="email" />
+                    <div className="mb-3">
+                        <label className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="email" value={state.email} onChange={(e) => handleChange(e)} />
 
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" />
+                    <div className="mb-3">
+                        <label className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password" value={state.password} onChange={(e) => handleChange(e)} />
                     </div>
-                    <div class="mb-3">
-                        <label class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control" id="conpassword" />
+                    <div className="mb-3">
+                        <label className="form-label">Confirm Password</label>
+                        <input type="password" className="form-control" id="conpassword" value={state.conpassword} onChange={(e) => handleChange(e)} />
                     </div>
-                    <button type="submit" class="btn btn-dark">Submit</button>
+                    <button type="submit" className="btn btn-dark" onClick={(e) => handleSubmit(e)}>Submit</button>
                 </form>
             </div>
         </div>
