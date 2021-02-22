c008970f0a9abaf2c424a1d9052817597dd40743
MakarandPundlik <mmp2308@gmail.com>
2021-02-22 21:42:43 +0530

    added users on homepage

--- frontend_mod/src/components/Homepage.jsx
+++ frontend_mod/src/components/Homepage.jsx
@@ -2,8 +2,8 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 const API_URL = "http://localhost:2020/"
 function Homepage(props) {
-    let [users,setUsers] = useState([]);
-    useEffect(async()=>{
+    const [users,setUsers] = useState([]);
+    const getUsers=async()=>{
         await axios.get(API_URL,{
             headers:{
                 'Content-Type': 'application/json',
@@ -13,18 +13,55 @@ function Homepage(props) {
         })
         .then((res)=>{
            const data = res.data.users;
-          
-           setUsers(users=>[...users,data]);
+            console.log(data);
+           setUsers([...users,data]);
            console.log(users);
+           
         })
         .catch((err)=>{
             console.log(err);
         })
+    }
+    useEffect(()=>{
+        
+        getUsers();
     },[])
     return (
-        <div>
-            
+        users?(<div style={{ marginTop: '25%' }}>
+        <div className="spinner-border" role="status">
         </div>
+        </div>):(
+            <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        )
     );
 }
 
