be441892037074ffa869e0e1f5861a090aa9ee8a
MakarandPundlik <mmp2308@gmail.com>
2021-02-25 19:36:22 +0530

    added blog middleware

--- backend_mod/middlewares/addBlog.js
+++ backend_mod/middlewares/addBlog.js
@@ -2,17 +2,20 @@ const userSchema = require('../models/user');
 
 module.exports.addBlog=(req,res)=>{
     const {title,data} = req.body.blog;
-    const {email} = req.body;
-    userSchema.findOne({email})
-    .then((res)=>{
-        console.log("User found")
-        console.log(res)
-        
-    })
-    .catch((err)=>{
-        console.log("Something wend wrong")
-    })
-    
     console.log(title,data);
+    const {email} = req.body;
+    const newblog ={
+        title,
+        data
+    }
+    userSchema.findOneAndUpdate(
+        {email},
+        {$push:{blogArray:newblog}},
+        {upsert:true},
+        {useFindAndModify:false}
+        )
+        .then(res=>console.log(res))
+        .catch(err=>console.log(err));
+   
     return res.json({msg:"Blog added successfully"});
 }
