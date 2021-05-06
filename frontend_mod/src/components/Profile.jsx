c52869840dc39e4eae1dc02d641a35a16a7f00c2
MakarandPundlik <mmp2308@gmail.com>
2021-05-06 20:53:58 +0530

    updated components

--- frontend_mod/src/components/Profile.jsx
+++ frontend_mod/src/components/Profile.jsx
@@ -1,38 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import ImageArray from './ImagesData';
-import upload_image from '../images/upload_image.png'
-function Profile({
-  total,name
-}) {
-    const [state,setState] = useState({
-        profilepic:null
-    })
+import React, { useEffect, useState } from "react";
+import ImageArray from "./ImagesData";
+import upload_image from "../images/upload_image.png";
+function Profile({ total, name }) {
+  const [state, setState] = useState({
+    profilepic: null,
+  });
+
+  const handleFileChange = (e) => {
+    setState({ ...state, profilepic: e.target.files[0] });
+    console.log(state.profilepic);
+  };
+  return (
     
-   
-    const handleFileChange=(e)=>{
-      setState({...state,profilepic:e.target.files[0]})
-      console.log(state.profilepic);
-    }
-    return (
-        <div className="row">
-          <div className="card m-3" style={{width:"20rem"}}>
+      <div className="card" style={{maxWidth:"30rem"}}>
+        <h5 className="card-title m-2">
+          {name.toLocaleUpperCase()}
+        </h5>
         <div className="text-center m-3">
-            {
-                state.profilepic===null?
-                <div><img src={upload_image} className="rounded-circle" height="200px" width="200px" alt="..."/><input type="file"/><button onClick={handleFileChange}>Upload</button></div>:
-                <img src={state.profilepic} className="rounded-circle" height="200px" width="200px" alt="..."/>
-            }
-           
+          {state.profilepic === null ? (
+            <div>
+              <img
+                src={upload_image}
+                className="rounded-circle"
+                height="200px"
+                width="200px"
+                alt="..."
+              />
+              <input type="file" />
+              <button onClick={handleFileChange}>Upload</button>
+            </div>
+          ) : (
+            <img
+              src={state.profilepic}
+              className="rounded-circle"
+              height="20rem"
+              width="20rem"
+              alt="..."
+            />
+          )}
         </div>
+        <hr className="m-3"/>
         <div className="card-body">
-          <h5 className="card-title">{name.toLocaleUpperCase()}</h5>
-          
+          <h5 className="card-title">Total blogs written : {total}</h5>
         </div>
-        
+        <hr className="m-3"/>
+        <div className="card-body">
+          <h5 className="card-title">Total views : 10</h5>
+        </div>
       </div>
-     
-        </div>
-    );
+   
+  );
 }
 
 export default Profile;
