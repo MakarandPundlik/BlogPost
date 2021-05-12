a4804c5a7d3f81a49f151cba3c233b8926fd08b2
MakarandPundlik <mmp2308@gmail.com>
2021-05-12 11:59:34 +0530

    added background image

--- frontend_mod/src/components/Homepage.jsx
+++ frontend_mod/src/components/Homepage.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import landing_2 from '../images/landing_2.jpg';
+import React from "react";
+
 function Homepage(props) {
-    return (
-        <div class="container-fluid has-bg-img" style={{marginTop:"5%"}}>
-       
-        
+  return (
+    <div className="container-fluid" style={{marginTop:"5%"}}>
+      
+      <div className="waveWrapper waveAnimation">
+        <div className="waveWrapperInner bgTop">
+          <div
+            className="wave waveTop"
+            style={{
+              backgroundImage:
+                "url(" +
+                "http://front-end-noobs.com/jecko/img/wave-top.png" +
+                ")",
+            }}
+          ></div>
+        </div>
+
+        <div className="waveWrapperInner bgMiddle">
+          <div
+            className="wave waveMiddle"
+            style={{
+              backgroundImage:
+                "url(" +
+                "http://front-end-noobs.com/jecko/img/wave-mid.png" +
+                ")",
+            }}
+          ></div>
+        </div>
+        <div className="waveWrapperInner bgBottom">
+          <div
+            className="wave waveBottom"
+            style={{
+              backgroundImage:
+                "url(" +
+                "http://front-end-noobs.com/jecko/img/wave-bot.png" +
+                ")",
+            }}
+          ></div>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Homepage;
+
+//  backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")",
